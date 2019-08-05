295ec78b34974517d4872851f64e622ed11dea38
said moutaa <smoutaa@gmail.com>
2019-08-06 00:50:38 +0200

    Adding validation and login end point

--- react-social-app/social-app-functions/functions/index.js
+++ react-social-app/social-app-functions/functions/index.js
@@ -14,7 +14,7 @@ const config = {
     storageBucket: "react-social-app-e3b9e.appspot.com",
     messagingSenderId: "716185493921",
     appId: "1:716185493921:web:3dcbf8f47e0f0685"
-  };
+};
 
 const firebase = require('firebase');
 firebase.initializeApp(config);
@@ -28,52 +28,69 @@ const db = admin.firestore();
 
 // First API function
 exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello Said!");
+    response.send("Hello Said!");
 });
 
 
 // Second API
-app.get('/screams', (req,res)=>{
+app.get('/screams', (req, res) => {
     db
-    .collection('screams')
-    .orderBy('createdAt','desc')
-    .get()
-    .then((data)=>{
-        let screams =[];
-        data.forEach((doc)=> {
-        screams.push(
-            {screamId: doc.id,
-             ...doc.data() 
-            }
-        );
-    });
-       return res.json(screams); 
-    }).catch(err => console.log(err));
+        .collection('screams')
+        .orderBy('createdAt', 'desc')
+        .get()
+        .then((data) => {
+            let screams = [];
+            data.forEach((doc) => {
+                screams.push({
+                    screamId: doc.id,
+                    ...doc.data()
+                });
+            });
+            return res.json(screams);
+        }).catch(err => console.log(err));
 })
- 
+
 // Third API function:
-  app.post('/scream',(req, res) => {
+app.post('/scream', (req, res) => {
 
-        const newScream = {
-            body: req.body.body,
-            userHandle: req.body.userHandle,
-            createdAt: new Date().toISOString()
-        };
+    const newScream = {
+        body: req.body.body,
+        userHandle: req.body.userHandle,
+        createdAt: new Date().toISOString()
+    };
 
-        db
+    db
         .collection('screams')
         .add(newScream)
-        .then((doc)=>{
-            res.json({message: ` The ${doc.id} was created successfully`});
+        .then((doc) => {
+            res.json({
+                message: ` The ${doc.id} was created successfully`
+            });
         })
-        .catch((err)=>{
-            res.status(500).json({error: ` Someting went wrong`});
+        .catch((err) => {
+            res.status(500).json({
+                error: ` Someting went wrong`
+            });
             console.error(err);
-        });   
-   });
+        });
+});
+
+const isEmpty = (string) => {
+    if (string.trim() === '')
+        return true;
+    else
+        return false;
+}
+
+const isEmail = (email) => {
+    const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (email.match(regEx)) return true;
+    else return false;
+};
+
 
 //Signup route
-app.post('/signup', (req,res) => {
+app.post('/signup', (req, res) => {
     const newUser = {
         email: req.body.email,
         password: req.body.password,
@@ -81,48 +98,96 @@ app.post('/signup', (req,res) => {
         handle: req.body.handle,
     };
 
-    //TODO: validate data
+    let errors = {};
+
+    if (isEmpty(newUser.email)) {
+        errors.email = 'Must not be empty';
+    } else if(!isEmail(newUser.email)){
+        errors.email= 'Must be a valid email address'
+    }
+
+    if(isEmpty(newUser.password)) errors.password = 'Must not be Empty';
+    if(newUser.password!== newUser.confirmPassword) errors.confirmPassword = 'Passwords must match';
+    if(isEmpty(newUser.handle)) errors.handle= 'Must not be Empty';
+    if(Object.keys(errors).length>0) return res.status(400).json(errors);
+
+    
     let token, userId;
     db.doc(`/users/${newUser.handle}`)
-    .get()
-    .then(doc=>{
-        if(doc.exists){
-            return res.status(400).json({handle: `This handle is already taken`});
-        }
-        else{
-           
-            return  firebase
-            .auth()
-            .createUserWithEmailAndPassword(newUser.email, newUser.password);
-        }
-    })
-    .then((data)=>{
-        userId= data.user.uid;
-        return  data.user.getIdToken();
-    })
-    .then((idtoken) =>{
-        token =idtoken;
-      //   return res.status(201).json({ token });
-      const userCredentials ={
-          handle:  newUser.handle,
-          email: newUser.email,
-          createdAt: new Date().toISOString(),
-          userId: userId
-      }
-      db.doc(`/users/${newUser.handle}`).set(userCredentials);
-    })
-    .then(()=>{
-        return res.status(201).json({token});
-    })
-    .catch((err) =>{
-        console.error(err);
-        if(err.code=== 'auth/email-already-in-use'){
-            return res.status(400).json({email:'Email already in use'})
-        }
-        else{
-        res.status(500).json({error: err.code});
-        }
-    });
+        .get()
+        .then(doc => {
+            if (doc.exists) {
+                return res.status(400).json({
+                    handle: `This handle is already taken`
+                });
+            } else {
+
+                return firebase
+                    .auth()
+                    .createUserWithEmailAndPassword(newUser.email, newUser.password);
+            }
+        })
+        .then((data) => {
+            userId = data.user.uid;
+            return data.user.getIdToken();
+        })
+        .then((idtoken) => {
+            token = idtoken;
+            //   return res.status(201).json({ token });
+            const userCredentials = {
+                handle: newUser.handle,
+                email: newUser.email,
+                createdAt: new Date().toISOString(),
+                userId: userId
+            }
+            db.doc(`/users/${newUser.handle}`).set(userCredentials);
+        })
+        .then(() => {
+            return res.status(201).json({
+                token
+            });
+        })
+        .catch((err) => {
+            console.error(err);
+            if (err.code === 'auth/email-already-in-use') {
+                return res.status(400).json({
+                    email: 'Email already in use'
+                })
+            } else {
+                res.status(500).json({
+                    error: err.code
+                });
+            }
+        });
 });
 
-   exports.api=functions.https.onRequest(app);
+//Add the login function
+app.post('/login', (req, res) => {
+    const user = {
+      email: req.body.email,
+      password: req.body.password
+    };
+    let errors = {};
+    if (isEmpty(user.email)) errors.email = 'Must not be empty';
+    if (isEmpty(user.password)) errors.password = 'Must not be empty';
+    if (Object.keys(errors).length > 0) return res.status(400).json(errors);
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(user.email, user.password)
+      .then((data) => {
+        return data.user.getIdToken();
+      })
+      .then((token) => {
+        return res.json({ token });
+      })
+      .catch((err) => {
+        console.error(err);
+        if (err.code === 'auth/wrong-password') {
+          return res
+            .status(403)
+            .json({ general: 'Wrong credentials, please try again' });
+        } else return res.status(500).json({ error: err.code });
+      });
+  });
+  
+exports.api = functions.https.onRequest(app);
